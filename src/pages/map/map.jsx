9c0fd1fd9f4046f255c7345ac8f7a1d2204fa26b
xiayuushi@qq.com <l>
2021-04-03 01:07:46 +0800

    地图模块-租房信息、地图标注移动到可视范围中心点

--- src/pages/map/map.jsx
+++ src/pages/map/map.jsx
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import './map.css'
 
 import store from '../../store'
-
+import { BASEURL } from '../../utils/base_url'
 import { Toast } from 'antd-mobile'
 
 // 从window取出BMap对象，以便在当前组件全局使用
 const BMap = window.BMap
 
 class Map extends Component {
-  state = {
-    currentCityInfo: store.getState()
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentCityInfo: store.getState(),
+      sClass: 'houseList',
+      houseList: []
+    }
   }
 
   componentDidMount () {
@@ -23,6 +28,13 @@ class Map extends Component {
 
     // 注意：在调用地图实例化API时，传入容器盒子id属性值不需要带'#'
     this.map = new BMap.Map('container')
+
+    // 移动地图时，关闭（第三级覆盖物标注）下方信息面板
+    this.map.addEventListener('movestart', () => {
+      this.setState({
+        sClass: 'houseList'
+      })
+    })
 
     // 创建地址解析器实例
     var myGeo = new BMap.Geocoder()
@@ -59,7 +71,6 @@ class Map extends Component {
     // 第三缩放级别的地图标注与之前的不同
     if (this.scaleLevel !== 16) {
       this.map.centerAndZoom(point, this.scaleLevel)
-
       Toast.loading('正在加载...')
 
       // 获取房源信息
@@ -68,7 +79,7 @@ class Map extends Component {
       })
 
       // 遍历服务器数据，生成地图标注
-      res.body.map(item => {
+      res.body.forEach(item => {
         // 服务器返回的经纬度数据转成百度地图的中心坐标点
         let point = new BMap.Point(item.coord.longitude, item.coord.latitude)
 
@@ -90,7 +101,8 @@ class Map extends Component {
           backgroundColor: 'transparent'
         })
 
-        this.map.addOverlay(label) // 根据上面的参数生成地图覆盖物(标注)
+        // 根据上面的参数生成地图覆盖物(标注)
+        this.map.addOverlay(label)
 
         Toast.hide()
 
@@ -108,7 +120,7 @@ class Map extends Component {
         url: `/area/map?id=${id}`
       })
 
-      res.body.map(item => {
+      res.body.forEach(item => {
         let point = new BMap.Point(item.coord.longitude, item.coord.latitude)
 
         let opts = {
@@ -131,25 +143,59 @@ class Map extends Component {
 
         Toast.hide()
 
-        label.addEventListener('click', () => {
-          // this.fnRefreshOverlay(point, item.value)
+        label.addEventListener('click', e => {
+          let moveX, moveY
+          // 防止代码出错做的平稳退化
+          try {
+            // 光标坐标位置
+            let { clientX, clientY } = e.changedTouches[0]
+            // 计算光标移动到屏幕中心点的坐标
+            moveX = window.innerWidth / 2 - clientX
+            moveY = window.innerHeight / 4 - clientY
+          } catch (error) {
+            // 光标坐标位置
+            let { clientX, clientY } = e
+            // 计算光标移动到屏幕中心点的坐标
+            moveX = window.innerWidth / 2 - clientX
+            moveY = window.innerHeight / 4 - clientY
+          }
+          this.fnShowHouseList(item.value, { moveX, moveY })
         })
       })
     }
   }
-  // 封装方法加载子级覆盖物标注
+  // 点击第三级标注展开租房信息
+  fnShowHouseList = async (value, obj) => {
+    // 点击标注时，当前标注移动到（地图可视区的）屏幕中心点
+    this.map.panBy(obj.moveX, obj.moveY)
+
+    Toast.loading('加载中...')
+    let res = await this.$request({
+      url: `/houses?cityId=${value}`
+    })
+    Toast.hide()
+
+    this.setState({
+      sClass: 'houseList houseListShow',
+      houseList: res.body.list
+    })
+  }
+  // 封装方法加载子级覆盖物标注(删除之前存在的覆盖物、重新生成)
   fnRefreshOverlay = (point, id) => {
-    // 删除之前存在的地图(使用定时器异步包裹清除覆盖物的代码,否则删除过快会报错)
+    // 删除之前存在的地图标注(使用定时器异步包裹清除覆盖物的代码,否则删除过快会报错)
     setTimeout(() => {
       this.map.clearOverlays()
     }, 0)
+
     // 递归调用生成标注的fnAddOverlay方法(重新生成子级标注)
     this.fnAddOverlay(point, id)
   }
 
   render () {
+    let { sClass, houseList } = this.state
     return (
       <div>
+        {/* 顶部 */}
         <div className='common_title'>
           <span
             className='back iconfont icon-prev'
@@ -157,8 +203,45 @@ class Map extends Component {
           ></span>
           <h3>地图找房</h3>
         </div>
+        {/* 地图 */}
         <div className='map_com'>
           <div id='container' style={{ width: '100%', height: '100%' }}></div>
+        </div>
+        {/* 房屋信息列表 */}
+        <div className={sClass}>
+          <div className='titleWrap'>
+            <h1 className='listTitle'>房屋列表</h1>
+            <a className='titleMore' href='/house/list'>
+              更多房源
+            </a>
+          </div>
+          {houseList.map(item => (
+            <div className='houseItems' key={item.houseCode}>
+              <div className='house'>
+                <div className='imgWrap'>
+                  <img
+                    className='img'
+                    src={`${BASEURL}${item.houseImg}`}
+                    alt={item.title}
+                  />
+                </div>
+                <div className='content'>
+                  <h3 className='title'>{item.title}</h3>
+                  <div className='desc'>{item.desc}</div>
+                  <div>
+                    {item.tags.map((v, i) => (
+                      <span className={`tag tag${i}`} key={v}>
+                        {v}
+                      </span>
+                    ))}
+                  </div>
+                  <div className='price'>
+                    <span className='priceNum'>{item.price}</span> 元/月
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     )
