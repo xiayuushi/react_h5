d2d1b2031e3c092be4367b38c84d2e8f1ea7f57f
xiayuushi@qq.com <l>
2021-04-16 09:38:02 +0800

    打包优化-UI按需加载减小打包生成的文件大小、路由组件lazy懒加载加快渲染速度

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense, lazy } from 'react'
 
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 
-import Layout from './pages/layout/layout.jsx'
-import Map from './pages/map/map.jsx'
-import Detail from './pages/detail/detail.jsx'
-import Login from './pages/login/login.jsx'
-import Rent from './pages/rent/rent.jsx'
-import Rentlist from './pages/rentlist/rentlist.jsx'
 import { withRouter } from 'react-router-dom'
+
+// 首屏必须加载的路由组件必须使用正常导入方式
+// 否则会报错：Import in body of module; reorder to top  import/first
+import Layout from './pages/layout/layout.jsx'
+
+// 除首屏必须加载的路由组件外，其余路由组件均使用lazy懒加载优化项目（打包）
+const Map = lazy(() => import('./pages/map/map'))
+const Detail = lazy(() => import('./pages/detail/detail'))
+const Login = lazy(() => import('./pages/login/login'))
+const Rent = lazy(() => import('./pages/rent/rent'))
+const Rentlist = lazy(() => import('./pages/rentlist/rentlist'))
 
 const WithRent = withRouter(Rent)
 const WithLogin = withRouter(Login)
@@ -17,26 +22,28 @@ class App extends Component {
   render () {
     return (
       <HashRouter>
-        <Switch>
-          <Route path='/layout' component={Layout} />
-          <Route path='/map' component={Map} />
-          <Route path='/login' component={Login} />
-          <Route path='/rentlist' component={Rentlist} />
-          {/* 路由拦截，没有登录态不让用户进入Rent组件 */}
-          <Route
-            path='/rent'
-            render={() => {
-              let token = localStorage.getItem('haoke_token')
-              if (token) {
-                return <WithRent />
-              } else {
-                return <WithLogin />
-              }
-            }}
-          />
-          <Route path='/detail/:key' component={Detail} />
-          <Redirect from='/' to='/layout' exact></Redirect>
-        </Switch>
+        <Suspense fallback={<div>...</div>}>
+          <Switch>
+            <Route path='/layout' component={Layout} />
+            <Route path='/map' component={Map} />
+            <Route path='/login' component={Login} />
+            <Route path='/rentlist' component={Rentlist} />
+            {/* 路由拦截，没有登录态不让用户进入Rent组件 */}
+            <Route
+              path='/rent'
+              render={() => {
+                let token = localStorage.getItem('haoke_token')
+                if (token) {
+                  return <WithRent />
+                } else {
+                  return <WithLogin />
+                }
+              }}
+            />
+            <Route path='/detail/:key' component={Detail} />
+            <Redirect from='/' to='/layout' exact></Redirect>
+          </Switch>
+        </Suspense>
       </HashRouter>
     )
   }
--- src/pages/layout/layout.jsx
+++ src/pages/layout/layout.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, lazy } from 'react'
 import './layout.css'
 
-import Home from '../home/home'
-import Houselist from '../houselist/houselist'
-import Info from '../info/info'
-import User from '../user/user'
 import { Route, Link, Redirect, Switch } from 'react-router-dom'
+
+import Home from '../home/home'
+const Houselist = lazy(() => import('../houselist/houselist'))
+const Info = lazy(() => import('../info/info'))
+const User = lazy(() => import('../user/user'))
 
 // 自定义路由，让当前所在的路由高亮显示
 function CustomLink ({ label, to, iconClass }) {
